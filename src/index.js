4ef6d971b6717578971e3dcf0c96ec5a5ca0bb4e
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2021-10-16 17:55:58 -0700

    Forecast refactor
    Refactoring functions to create the five day forecast

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
+let apiKey = "e9bb26ed626e12b32c5d3d0d23619b61";
+let forecastDateString = null;
 let form = document.querySelector("#search-bar");
 let celsius = document.querySelector("#celsius");
 let celsiusTemp = null;
+let forecastDate = null;
 let fahrenheitElement = document.querySelector("#fahrenheit");
 let fahrenheitTemp = null;
+let fullDate = null;
+let monthAbb = null;
+let now = null;
+let respDt = null;
 let temperatureElement = document.querySelector("#current-temp");
+let weekDay = null;
 let windElement = document.querySelector(".current-wind");
 
+function getWeekDay(dtStamp) {
+  fullDate = new Date(dtStamp * 1000);
+  weekDay = fullDate.toLocaleString("en-US", { weekday: "long" });
+}
+
+function getWeekDate(dtStamp) {
+  fullDate = new Date(dtStamp * 1000);
+  monthAbb = fullDate.getMonth();
+  monthAbb += 1;
+  forecastDate = fullDate.getDate();
+}
+
 function getLastUpdatedTime(response) {
+  getWeekDay(response.data.dt);
   let currentTime = document.querySelector("#current-time");
-  let respDt = response.data.dt;
   var ampm = "AM";
-  let now = new Date(respDt * 1000);
-  let weekday = now.toLocaleString("en-US", { weekday: "long" });
-  let hours = now.getHours();
+  let hours = fullDate.getHours();
   var ampm = "Checking time...";
   if (hours >= 12) {
     hours = hours - 12;
@@ -23,17 +41,25 @@ function getLastUpdatedTime(response) {
   } else {
     ampm = "AM";
   }
-  let minutes = now.getMinutes();
+  let minutes = fullDate.getMinutes();
   let formattedMins = minutes.toLocaleString("en-US", {
     minimumIntegerDigits: 2,
     useGrouping: false,
   });
-  currentTime.innerHTML = `As of ${weekday}, ${hours}:${formattedMins} ${ampm}`;
+  currentTime.innerHTML = `As of ${weekDay}, ${hours}:${formattedMins} ${ampm}`;
+}
+
+function getForecast(coordinates) {
+  let lat = coordinates.lat;
+  let lon = coordinates.lon;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&unit=imperial`;
+  axios.get(apiUrl).then(displayWeekForecast);
 }
 
 function displayCurrentTemp(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#current-city");
+  coordinates = response.data.coord;
   let description = response.data.weather[0].description;
   let descriptionElement = document.querySelector(".current-condition");
   let iconElement = document.querySelector("#current-icon");
@@ -50,11 +76,11 @@ function displayCurrentTemp(response) {
   );
   iconElement.setAttribute("alt", description);
   windElement.innerHTML = `Wind Speed: ${windSpeed} mph`;
-  displayForecast();
+  displayTodaysForecast();
+  getForecast(response.data.coord);
 }
 
 function search(userCity) {
-  let apiKey = "e9bb26ed626e12b32c5d3d0d23619b61";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayCurrentTemp);
   axios.get(apiUrl).then(getLastUpdatedTime);
@@ -84,12 +110,14 @@ function displayFahrenheitTemp(temp) {
   fahrenheit.classList.add("active");
 }
 
-function displayForecast() {
+function displayTodaysForecast() {
   let forecastElement = document.querySelector(".today-weather");
   let forecastHTML = "";
-  forecastHTML =
-    forecastElement.innerHTML +
-    `
+  let dayTimes = ["Morning", "Afternoon", "Evening", "Overnight"];
+  dayTimes.forEach(function (time) {
+    forecastHTML =
+      forecastElement.innerHTML +
+      `
 					<table class="table table-borderless">
 						<thead>
 							<tr class="today-time-grid">
@@ -115,7 +143,23 @@ function displayForecast() {
 						</tbody>
 					</table>
 				</div>	`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function displayWeekForecast(response) {
+  let forecastElement = document.querySelector(".weekday-forecast");
+  let forecastIcon = document.querySelector(".forecast-icons");
+  let forecastTemp = document.querySelector(".forecast-temp");
+  let forecast = response.data.daily;
+  let weekdayName = document.querySelector(".weekday");
+
+  forecast.forEach(function (day) {
+    getWeekDay(day.dt);
+    getWeekDate(day.dt);
+    forecastElement.innerHTML = `<th scope="col" class="weekDay">${weekDay}<span class="weekDate">${monthAbb}/${forecastDate}</span></th>`;
+  });
 }
 
 form.addEventListener("submit", submitSearch);
